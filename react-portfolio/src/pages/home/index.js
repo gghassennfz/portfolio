8ffefd1726102c5c7d709738aef50cb250c76e5b
Ghassen Nefzi <72090039+gghassennfz@users.noreply.github.com>
2024-10-27 05:45:29 +0100

    Updated multiple files latest modifications

--- react-portfolio/src/pages/home/index.js
+++ react-portfolio/src/pages/home/index.js
@@ -19,10 +19,7 @@ export const Home = () => {
            className="h_bg-image order-1 order-lg-2 h-100"
            style={{
              backgroundImage: `url(${introdata.your_img_url})`,
-             backgroundSize: "contain", // Adjusts the image to fit within the div while preserving aspect ratio
-             backgroundPosition: "center", // Centers the image in the div
-             backgroundRepeat: "no-repeat" // Ensures the image is not repeated if it doesn't fill the container
-           }}
+             git }}
           ></div>
           <div className="text order-2 order-lg-1 h-100 d-lg-flex justify-content-center">
             <div className="align-self-center ">
